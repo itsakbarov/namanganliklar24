3273c5392376b55015e0c5bf126ae796e6d30c81
theAkbarov <hawk@hawk.com>
2020-10-27 16:59:36 +0500

    Namanganliklar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#subscribe_btn').click(function() {
+        $('.telegram_modal').show(200);
+    });
+    $('.close-btn').click(function() {
+        $('.telegram_modal').hide(200);
+    });
+    
+});
